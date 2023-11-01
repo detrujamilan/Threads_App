abc290f0ed90be1daa6b99dd76e5e5ea0ab84daf
milandetruja <milandetruja2@gmail.com>
2023-11-01 22:46:33 +0530

    button tab create

--- StackNavigator.js
+++ StackNavigator.js
@@ -3,10 +3,34 @@ import { NavigationContainer } from "@react-navigation/native";
 import Login from "./screens/Login";
 import Register from "./screens/Register";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import { Entypo , AntDesign } from '@expo/vector-icons';
 
 const StackNavigator = () => {
   const Stack = createNativeStackNavigator();
+  const Tab = createNativeStackNavigator();
 
+  const ButtonTabs = () => {
+    return (
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: "Home",
+            tabBarLabelStyle: { color: "black" },
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Entypo name="home" size={24} color="black" />
+              ) : (
+                <AntDesign name="home" size={24} color="black" />
+              ),
+          }}
+        />
+      </Tab.Navigator>
+    );
+  };
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -18,6 +42,11 @@ const StackNavigator = () => {
         <Stack.Screen
           name="Register"
           component={Register}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
--- screens/Login.js
+++ screens/Login.js
@@ -8,39 +8,57 @@ import {
   Pressable,
   ToastAndroid,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { MaterialIcons, AntDesign } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import { baseUrl } from "../utils/utils";
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigation = useNavigation();
 
+  useEffect(() => {
+    const checkLocalStorage = () => {
+      try {
+        const token = AsyncStorage.getItem("token");
+        if (token) {
+          setTimeout(() => {
+            navigation.replace("Home");
+          }, 400);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    checkLocalStorage();
+  }, []);
+
   const handleLogin = () => {
     if (!email || !password) {
       ToastAndroid.show("Please enter all required fields", ToastAndroid.SHORT);
     }
-    const data = {
+    const bodyData = {
       email: email,
       password: password,
     };
     axios
-      .post(`${baseUrl}/login`, data)
+      .post(`${baseUrl}/login`, bodyData)
       .then((response) => {
         if (response.status === 200) {
           ToastAndroid.show("Login successfully", ToastAndroid.SHORT);
         }
-        ToastAndroid.show("Login successful", ToastAndroid.SHORT);
+        const token = response.data.token;
+        AsyncStorage.setItem("token", token);
+        navigation.navigate("Home");
       })
       .catch((error) => {
-        if (error.response.status === 400) {
-          ToastAndroid.show("Invalid Email & Password", ToastAndroid.SHORT);
-        }
+        ToastAndroid.show(error.response.data.message, ToastAndroid.SHORT);
       });
   };
+
   return (
     <SafeAreaView className="flex-1 items-center">
       <View className="mt-20">
